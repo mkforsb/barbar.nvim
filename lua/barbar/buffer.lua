c8ed7ce43e2d2ceecf85aa60742a2f3880ca9b03
Mikael Forsberg <mkforsb@gmail.com>
2024-09-07 18:09:42 +0200

    fix cwd offset

--- lua/barbar/buffer.lua
+++ lua/barbar/buffer.lua
@@ -92,7 +92,7 @@ function buffer.get_name(buffer_number, depth)
   if name ~= '' then
     local full_name = buf_get_option(buffer_number, 'buftype') == 'terminal' and
       terminalname(name) or (hide_extensions and fnamemodify(name, ':t') or name)
-    name = string.sub(full_name, #vim.loop.cwd())
+    name = string.sub(full_name, #vim.loop.cwd() + 2)
     -- local parts = split(full_name, separator)
     -- name = table_concat(list.slice_from_end(parts, depth), separator)
   elseif no_name_title ~= nil and no_name_title ~= vim.NIL then
